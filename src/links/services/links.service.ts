7cbd2d5d741d75d5538e95d6b79e685011ceaab5
mavi <mavi.baris96@gmail.com>
2022-02-01 03:34:36 +0300

    Add convertDeeplinkToUrl

--- src/links/services/links.service.ts
+++ src/links/services/links.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { DeeplinkToUrlLogicalService } from '../converter/deeplinkToUrl/deeplinkToUrl.service';
 import { UrlToDeeplinkLogicalService } from '../converter/urlToDeeplink/urlToDeeplink.service';
+import { FromDeeplinkToUrlDto } from '../dtos/FromDeeplinkToUrl.dto';
 import { FromUrlToDeeplinkDto } from '../dtos/FromUrlToDeeplink.dto';
 
 import { Converter } from '../entities/links.entity';
@@ -10,6 +12,7 @@ import { Converter } from '../entities/links.entity';
 export class LinksService {
   constructor(
     private urlToDeeplinkLogicalService: UrlToDeeplinkLogicalService,
+    private deeplinkToUrlLogicalService: DeeplinkToUrlLogicalService,
     @InjectRepository(Converter) private repo: Repository<Converter>,
   ) {}
 
@@ -18,6 +21,15 @@ export class LinksService {
   ): Promise<FromUrlToDeeplinkDto> {
     this.urlToDeeplinkLogicalService.urlToDeeplinkService(fromUrlToDeeplinkDto);
     const links = this.repo.create(fromUrlToDeeplinkDto);
+    console.log(links);
+    return await this.repo.save(links);
+  }
+
+  async convertDeeplinkToUrl(
+    fromDeeplinkToUrlDto: FromDeeplinkToUrlDto,
+  ): Promise<FromDeeplinkToUrlDto> {
+    this.deeplinkToUrlLogicalService.deeplinkToUrlService(fromDeeplinkToUrlDto);
+    const links = this.repo.create(fromDeeplinkToUrlDto);
     console.log(links);
     return await this.repo.save(links);
   }
