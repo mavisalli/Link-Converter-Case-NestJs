ade2ad542225442123426c367c71a0852f036956
mavi <mavi.baris96@gmail.com>
2022-02-01 03:32:15 +0300

    Update enums

--- src/links/converter/constant/constants.ts
+++ src/links/converter/constant/constants.ts
@@ -19,5 +19,23 @@ const enum Constant {
   AMPERSAND_CAMPAIGN_ID_EQ = '&CampaignId=',
 
   SEARCH_PARAM = '/sr',
+
+  //use inside deeplinkToUrl folder
+
+  HOMEPAGE_LINK = 'https://www.trendyol.com',
+  HOMEPAGE_SR = 'https://www.trendyol.com/sr?q=',
+  HOMEPAGE_BRAND = 'https://www.trendyol.com/brand/name-p-',
+
+  PRODUCT_CONTENTID = 'Product&ContentId=',
+
+  AMPERSAND_CAMPAIGN_ID = '&CampaignId',
+
+  Q_BOUTIQUE_ID = '?boutiqueId',
+
+  AMPERSAND_MERCHANT_ID_LOWER = '&merchantId',
+  AMPERSAND_MERCHANT_ID = '&MerchantId',
+  Q_MERCHANT_ID = '?merchantId',
+
+  SEARCH_QUERY = 'Search&Query=',
 }
 export default Constant;
