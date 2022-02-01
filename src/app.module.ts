9bc07ed7896f1f600cbcd5adbb8dd3a60c8b50b7
mavi <mavi.baris96@gmail.com>
2022-02-01 03:10:56 +0300

    Add config info for connection to postgres and import links module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Converter } from './links/entities/links.entity';
+import { LinksModule } from './links/links.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.development.env',
+    }),
+
+    LinksModule,
+    TypeOrmModule.forRoot({
+      type: process.env.TYPE as any,
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [Converter],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
